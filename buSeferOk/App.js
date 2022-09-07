ad7b0b872b08f5747bca091eab3530cc6b2d3a86
Mert Öden <93067736+mertoden@users.noreply.github.com>
2022-09-08 01:43:14 +0300

    version 1

--- buSeferOk/App.js
+++ buSeferOk/App.js
@@ -1,112 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react";
+import { SafeAreaView, View, Text, Button, StyleSheet } from "react-native";
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  function say() {
+    console.log('merhaba');
+  }
+
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
+  <SafeAreaView style={styles.upper_container}>
+    <View style={styles.container}>
+      <Text>Component yapısı</Text>
+    </View>
+
+        <Button
+      onPress={say} 
+      title="Click Me!!"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"
+    />
+
+    <View style={styles.box}>
+      <Text>
+        MERT
       </Text>
     </View>
-  );
-};
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.bottom_container}>
+      <Text>
+        Burayı Okuyorsun
+      </Text>
+    </View>
+  </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  upper_container: {
+    flex: 1,
+    backgroundColor: "#6495ed",
+    flexDirection: "column",
+    //justifyContent "space between" uzaklaştırabildiği kadar compnentleri birbirinden uzaklaştırır.
+    //alignItems: x ekseninde hizalamaya yarar.
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    margin: 20,
+    padding: 20,
+    borderRadius: 20,
+  },
+  bottom_container: {
+    flex: 1,
+    backgroundColor: "blue",
+    margin: 20,
+    padding: 20,
+    borderRadius: 20,
+  },
+  box: {
+    flex: 1,
+    backgroundColor: "green",
+    margin: 20,
+    padding: 20,
+    borderRadius: 20,
+  }
+})
 export default App;
